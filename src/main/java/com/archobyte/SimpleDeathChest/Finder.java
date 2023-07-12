9786d000efdc9a0e22395bd3cd9d4d0e8f57e878
ArchoByte <archobyte@proton.me>
2023-07-13 01:16:45 +0300

    Prevent exception caused by unexpected death location
    Added logic which prevents exceptions in case of death under or over world build limits

--- src/main/java/com/archobyte/SimpleDeathChest/Finder.java
+++ src/main/java/com/archobyte/SimpleDeathChest/Finder.java
@@ -7,6 +7,7 @@ import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
 public class Finder {
+    // Directions in which search can be performed
     private static final Vector[] ADJACENT = {
             new Vector(0, 0, 1),
             new Vector(0, 1, 0),
@@ -17,14 +18,25 @@ public class Finder {
     };
 
     public static Location getNearestAirBlock(Location loc) {
-        Queue<Location> queue = new LinkedList<>();
+        Queue<Location> queue = new LinkedList<Location>();
         queue.add(loc);
         int maxY = loc.getWorld().getMaxHeight();
-        int minY = -64;
+        // All worlds have 0 min build height except "world"
+        int minY = 0;
+        if (loc.getWorld().getName() == "world")
+            minY = -64;
+        // If initial location is under the world limit
+        if (loc.getY() < minY)
+            loc.setY(minY);
+        // If initial location is over the world limit
+        if (loc.getY() > maxY)
+            loc.setY(maxY);
         while (!queue.isEmpty()) {
+            // Check current location
             Location current = queue.remove();
             if (current.getBlock().getType().isAir())
                 return current;
+            // Get next candidates for chech
             for (Vector dxyz : ADJACENT) {
                 Location next = new Location(loc.getWorld(),
                         current.getX() + dxyz.getX(),
